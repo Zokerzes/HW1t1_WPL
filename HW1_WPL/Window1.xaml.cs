01607ce71382d42e92a98b984a1176901b2cfd83
Roman <yozhik13@gmail.com>
2022-11-22 02:02:19 +0300

    ++ разметка калькулятора выполнениа

--- HW1_WPL/Window1.xaml.cs
+++ HW1_WPL/Window1.xaml.cs
@@ -10,25 +10,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace HW1_WPL
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Логика взаимодействия для Window1.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Window1 : Window
     {
-        public MainWindow()
+        public Window1()
         {
             InitializeComponent();
-        }
-
-        private void toNext_Click(object sender, RoutedEventArgs e)
-        {
-            Window1 window1 = new Window1();
-            window1.ShowDialog();
         }
     }
 }
